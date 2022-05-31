863c9d6b481fcbffba58a18d386ddd3779c20337
root <root@DESKTOP-26239RJ.localdomain>
2022-05-31 17:53:01 +0900

    sync

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, ChangeDetectorRef } from '@angular/core';
+
+import SwiperCore, { Navigation, Pagination, SwiperOptions } from "swiper";
+import { SwiperComponent } from 'swiper/angular';
+
+SwiperCore.use([Navigation, Pagination])
 
 @Component({
   selector: 'app-main',
@@ -6,11 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @ViewChild('swiperRef', { static: false }) swiperRef?: SwiperComponent;
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef, private ngZone: NgZone) { }
+
+  ngOnInit(): void {  }
+
+  config: SwiperOptions = {
+    slidesPerView: 'auto',
+    autoHeight: true,
+    pagination: {
+      clickable: true
+    },
+    navigation: true,
+    spaceBetween: 10
+  }
+
+  slidesEx = ['first', 'second']
+
+  onClick(){
+    console.log(`clicked`)
+  }
 
   onSwiper([swiper]: any){
     console.log(swiper);
   }
 
+  onSlideChange(swiper: any){
+    if(swiper.isEnd){
+      this.ngZone.run(() => {
+        this.slidesEx = [...this.slidesEx, `added ${this.slidesEx.length - 1}`];
+      });
+
+      console.log(this.slidesEx);
+    }
+  }
 }
